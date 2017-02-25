dbbc79857138778cd9522f879aebacd6e202e545
Maks Gulkovich <maks_tut@mail.ru>
2017-02-25 15:05:09 +0300

    Задание 1 Подготовка инфраструктуры

---
+++ Sel4Training/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Sel4Training
+{
+    [TestFixture]
+    public class OpenPage
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        [Test]
+        public void OpenPageTest()
+        {
+            driver.Navigate().GoToUrl("http://github.com");
+            wait.Until(ExpectedConditions.TitleContains("GitHub"));
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
