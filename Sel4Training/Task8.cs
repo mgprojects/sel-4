d2782940789246d6ba560a7a2b82e6eb4e766321
Maks Gulkovich <maks_tut@mail.ru>
2017-03-30 19:17:33 +0300

    Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров
    Сделайте сценарий, проверяющий наличие стикеров у всех товаров в учебном
    приложении litecart на главной странице. Стикеры -- это полоски в левом
    верхнем углу изображения товара, на которых написано New или Sale или
    что-нибудь другое. Сценарий должен проверять, что у каждого товара
    имеется ровно один стикер.

---
+++ Sel4Training/Task8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Sel4Training
+{
+    public class ProductsStickers
+    {
+        IWebDriver driver;
+        string mainPageUrl = "http://localhost/litecart/";
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [Test]
+        public void ProductsStickersTest()
+        {
+            driver.Navigate().GoToUrl(mainPageUrl);
+
+            var products = driver.FindElements(By.CssSelector(".middle > .content .box li")); // все товары
+            foreach (var item in products)
+            {
+                var stickers = item.FindElements(By.CssSelector(".sticker")); // все стикеры товара
+                Assert.That(stickers.Count + 1, Is.EqualTo(1), $"Неверное количество стикеров у товара '{item.Text}'");
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
